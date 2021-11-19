9b50baf75ffb69a16eaf7d3b762787104b06e67f
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2021-11-19 22:09:35 +0100

    Update ad-gruppe-oprettelse.ps1

--- ad-gruppe-oprettelse.ps1
+++ ad-gruppe-oprettelse.ps1
@@ -14,16 +14,17 @@ $antal = Read-host " "
     $fornavn = $fornavne.Split([Environment]::NewLine) | Get-Random
     $efternavn = $efternavne.Split([Environment]::NewLine) | Get-Random
     #Username
-    $part1 = $fornavn.Substring(0,3).toLower().replace("Ã¸","o").replace("Ã¦","a").replace("Ã¥","a")
-    $part2 = $efternavn.Substring(0,3).toLower().replace("Ã¸","o").replace("Ã¦","a").replace("Ã¥","a")
-    $part3 = "01"
+    $part1 = $fornavn.Substring(0,3).toLower().replace("æ¸","a").replace("ø¦","o").replace("å","a")
+    $part2 = $efternavn.Substring(0,3).toLower().replace("æ¸","a").replace("ø¦","o").replace("å","a")
+    $part3 = 1
+    $part3+1
     $part4 = "@god.rm.dk"
     $navn = "$fornavn $efternavn"
     $username = $part1+$part2+$part3
     $email = $part1+$part2+$part3+$part4
-    Write-Host "Navn:`t" $navn
+    Write-Host "Navn:`t`t" $navn
     Write-Host "username:`t" $username
-    Write-Host "email:`t" $email
+    Write-Host "email:`t`t" $email
     "";
 
 }
