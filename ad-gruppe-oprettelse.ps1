0899065e22dc32d75be920c05699f345b3432743
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2021-11-21 00:18:36 +0100

    test

--- ad-gruppe-oprettelse.ps1
+++ ad-gruppe-oprettelse.ps1
@@ -3,37 +3,63 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 
 
-
-#OU Oprettelse
-$ou = (invoke-webrequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/afdelinger.txt").content
-
+# Opret OU
+ # New-ADOrganizationalUnit -Name Users -Path OU=UK,DC=aplast3d,DC=com -ProtectedFromAccidentalDeletion $False
 
 
 
+# Bruger Oprettelse
+    $ou = (invoke-webrequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/afdelinger.txt").content
+        
+    #Bruger oprettelse
+    $fornavne = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/fornavne.txt").content
+    $Efternavne = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/efternavne.txt").content
 
-#Bruger oprettelse
-$fornavne = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/fornavne.txt").content
-$Efternavne = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/efternavne.txt").content
+    Write-host "antal" -NoNewline
+    $antal = Read-host " "
 
-Write-host "antal" -NoNewline
-$antal = Read-host " "
-
-1..$antal | % {
-    #Name
-    $fornavn = $fornavne.Split([Environment]::NewLine) | Get-Random
-    $efternavn = $efternavne.Split([Environment]::NewLine) | Get-Random
-    #Username
-    $part1 = $fornavn.Substring(0,3).toLower().replace("æ","a").replace("ø","o").replace("å","a")
-    $part2 = $efternavn.Substring(0,3).toLower().replace("æ","a").replace("ø","o").replace("å","a")
-    $part3 = 1
-    $part4 = "@god.rm.dk"
-    $navn = "$fornavn $efternavn"
-    $username = $part1+$part2+$part3
-    $email = $part1+$part2+$part3+$part4
+    1..$antal | % {
+        #Name
+        $fornavn = $fornavne.Split([Environment]::NewLine) | Get-Random
+        $efternavn = $efternavne.Split([Environment]::NewLine) | Get-Random
+        $navn = "$fornavn $efternavn"
+        #Username
+        $part1 = $fornavn.Substring(0,3).toLower().replace("æ","a").replace("ø","o").replace("å","a")+$efternavn.Substring(0,3).toLower().replace("æ","a").replace("ø","o").replace("å","a")
+        $part2 = 1
+        #email
+        $part4 = "@god.rm.dk"
+        $email = $part1+$part2+$part4
     
-    Write-Host "Navn:`t`t" $navn
-    Write-Host "username:`t" $username
-    Write-Host "email:`t`t" $email
-    "";
+        if(!(get-aduser -Filter 'Name -like "$part1*"')){$part2="0"+$part2}
+        else {$part2=1+(get-aduser -Filter 'Name -like "$part1*"').Name.Count;}
+        if($part2 -lt 10){$part2="0"+$part2}
+        $username = $part1+$part2
+
+        New-ADUser `
+        -Path OU=Users,OU=UK,DC=aplast3d,DC=com `
+        -Office Gødstrup Sygehus `
+        -Department Warehouse `
+        -Title Stock Worker `
+        -Name JUME `
+        -UserPrincipalName JUME@aplast3d.com `
+        -DisplayName Juan Mendoza `
+        -GivenName Juan `
+        -Surname Mendoza `
+        -AccountPassword (ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force) -PassThru| Enable-ADAccount
+        
+        Write-host `t`t`t"BRUGER: "$firstname $lastname "oprettes.." -f yellow
+
+        #Change password at logon
+        #Set-ADUser -Identity <samAccountName> -ChangePasswordAtLogon $true
 
 }
+
+
+
+
+
+
+    
+     }
+
+# Security Group oprettelse
