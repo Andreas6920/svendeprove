32ecfcacebfb12f9b4f768de7fa7560b879b6a26
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2021-12-14 17:08:49 +0100

    structuring

---
+++ powershellscripts/bruger-generator.ps1
@@ -0,0 +1,29 @@
+﻿
+# Bruger Oprettelse
+    $ou = (invoke-webrequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/afdelinger.txt").content
+        
+    #Bruger oprettelse
+    $fornavne = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/fornavne.txt").content
+    $Efternavne = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/svendeprove/main/efternavne.txt").content
+
+    Write-host "antal" -NoNewline
+    $antal = Read-host " "
+    Write-host "afdeling" -NoNewline
+    $afdeling = Read-host " "
+    cls
+    1..$antal | % {
+        #Name
+        $fornavn = $fornavne.Split([Environment]::NewLine) | Get-Random
+        $efternavn = $efternavne.Split([Environment]::NewLine) | Get-Random
+        $navn = "$fornavn $efternavn"
+        #Username
+        $part1 = $fornavn.Substring(0,3).toLower().replace("æ","a").replace("ø","o").replace("å","a")+$efternavn.Substring(0,3).toLower().replace("æ","a").replace("ø","o").replace("å","a")
+        $part2 = 1
+        #email
+        $part4 = "@god.rm.dk"
+        $email = $part1+$part2+$part4
+
+        Write-host $fornavn";"$efternavn";;"$afdeling""
+
+
+        }
